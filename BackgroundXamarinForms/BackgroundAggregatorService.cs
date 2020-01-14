fb9a46f2cd8eea501eba43bde9cad9f7af13a5d6
itcocr766@gmail.com <Intlog6151$%>
2020-01-13 21:36:15 -0600

    backgrounding

---
+++ BackgroundXamarinForms/BackgroundAggregatorService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BackgroundXamarinForms
+{
+    public class BackgroundAggregatorService
+    {
+        private static Dictionary<string, IBackgroundTask> _schedules = new Dictionary<string, IBackgroundTask>();
+        private static BackgroundAggregatorService _instance;
+
+        static BackgroundAggregatorService()
+        {
+        }
+
+        private BackgroundAggregatorService()
+        {
+        }
+
+
+        public static void Add<T>(Func<T> schedule) where T : IBackgroundTask
+        {
+            var typeName = schedule.GetType().GetGenericArguments()[0]?.Name;
+
+            if (typeName != null && !_schedules.ContainsKey(typeName))
+                _schedules.Add(typeName, schedule());
+        }
+
+
+        public static void StartBackgroundService()
+        {
+            var message = new StartLongRunningTask();
+            MessagingCenter.Send(message, nameof(StartLongRunningTask));
+        }
+
+
+        public static BackgroundAggregatorService Instance { get; } = _instance ?? (_instance = new BackgroundAggregatorService());
+
+        public void Start()
+        {
+            foreach (var schedule in _schedules)
+            {
+                schedule.Value.StartJob();
+            }
+        }
+
+
+    }
+}
---
+++ BackgroundXamarinForms/BackgroundSharedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackgroundXamarinForms
+{
+    public class BackgroundSharedService
+    {
+
+        private static BackgroundSharedService _instance;
+
+        static BackgroundSharedService()
+        {
+        }
+
+        private BackgroundSharedService()
+        {
+        }
+
+       
+        public static BackgroundSharedService Instance { get; } =
+            _instance ?? (_instance = new BackgroundSharedService());
+
+
+      
+        public void Start()
+        {
+            BackgroundAggregatorService.Instance.Start();
+        }
+    }
+}
